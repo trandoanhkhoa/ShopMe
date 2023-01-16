93fddf780ab5f1e78aafe011f4dc94cfb4614873
Trần Đỗ Anh Khoa <trandoanhkhoa192837@gmail.com>
2023-01-16 14:29:40 +0700

    first commit

---
+++ assets/Javascripts/main.js
@@ -0,0 +1,10 @@
+// var list_sld=["./assets/imgs/img1.webp","./assets/imgs/img2.webp","./assets/imgs/img3.webp","./assets/imgs/img4.webp","./assets/imgs/img5.webp"]
+
+// var i=0;
+// function carousel() {
+//     console.log(list_sld[i])
+//     $('.slider__img').attr('src',list_sld[i])
+//     setTimeout(carousel, 3000);   
+    
+//   }
+//   carousel();
